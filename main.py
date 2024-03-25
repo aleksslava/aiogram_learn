76cd2b1064023b83b9420372f501f493fce2ff7b
Vuacheslav <soloomonslava1991@gmail.com>
2024-03-25 12:44:45 +0400

    ''

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+from handlers import user_handlers, other_handlers
+
+logger = logging.getLogger(__name__)
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s'
+    )
+
+    logger.info('Starting Bot')
+
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.token,
+              parse_mode='HTML')
+
+    dp = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await  dp.start_polling(bot)
+
+asyncio.run(main())
+
+
+
